5e7b18b0d48e8948483ceb387cf5a48ced8a1a89
vcarlsberg <v.carlsberg@gmail.com>
2020-09-14 14:48:35 +0700

    change splitting data method

--- ARIMA-MLP_Parallel.R
+++ ARIMA-MLP_Parallel.R
@@ -2,18 +2,21 @@ library(xts)
 library(forecast)
 library(nnfor)
 library(TSrepr)
+library(TSstudio)
 
 flow_data<-read_data("Jakarta","K50000")
 flow_data_xts <- ts(flow_data[,3],start=c(flow_data[1,1], flow_data[1,2]), end=c(2019, 6), 
                    frequency=12)
 
-train_data<-window(flow_data_xts,1994,c(2018,12))
-test_data<-window(flow_data_xts,2019)
+train_test_data<-ts_split(flow_data_xts,sample.out=6)
 
-arima.model<-auto.arima(train_data)
-arima.model2<-auto.arima(train_data,lambda = "auto")
+#train_data<-window(flow_data_xts,1994,c(2018,12))
+#test_data<-window(flow_data_xts,2019)
 
-mlp.model<-mlp(as.ts(train_data),hd=c(5,7,8,3))
+arima.model<-auto.arima(train_test_data$train)
+arima.model2<-auto.arima(train_test_data$train,lambda = "auto")
+
+mlp.model<-mlp(as.ts(train_test_data$train),hd=c(5,7,8,3))
 
 length(mlp.model$fitted)
 length(train_data)
