cc04c671ae9f666346657295000408b73a25fca4
vcarlsberg <v.carlsberg@gmail.com>
2020-12-17 15:11:31 +0700

    reducing reps to 1 sel.lag on mlp made false

--- run_all_func.R
+++ run_all_func.R
@@ -45,19 +45,20 @@ for (flow in c("Outflow","Inflow"))
   }
 }
 
-for (flow in c("Outflow"))
+
+for (flow in c("Inflow"))
 {
-  for (location in c("Semarang"))
+  for (location in c("Jakarta"))
   {
-    for (denomination in c("K20000"))
+    for (denomination in c("K100000","K50000","K20000","K10000","K5000","K2000","K1000"))
     {
       for (preprocessing in c(1))
       {
-        for (MLP_layer in c(1))
+        for (MLP_layer in c(1,2))
         {
           tryCatch({
             print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLP_Individual"))
-            
+
             #findlag
             lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
             result<-MLP_Individual(preprocessing = preprocessing,
@@ -72,168 +73,125 @@ for (flow in c("Outflow"))
             print(e)
           })
 
-        }
-      }
-    }
-  }
-}
-
-for (flow in c("Outflow"))
-{
-  for (location in c("Semarang"))
-  {
-    for (denomination in c("K100000"))
-    {
-      for (preprocessing in c(1))
-      {
-        for (MLP_layer in c(2))
-        {
           tryCatch({
-            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_Individual"))
-            
-            #findlag
-            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMAX-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
-            result<-MLPX_Individual(preprocessing = preprocessing,
-                                   location=location,
-                                   denomination=denomination,
-                                   MLP_layer=MLP_layer,flow=flow,
-                                   lag=lagrow)
-            compiled_result<-rbind(compiled_result,result$modelResult)
-            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
-          },error=function(e){
-            print(e)
-          })
-          
-        }
-      }
-    }
-  }
-}
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLXP_Individual"))
 
-
-
-
-for (flow in c("Outflow"))
-{
-  for (location in c("Semarang"))
-  {
-    for (denomination in c("K100000"))
-    {
-      for (preprocessing in c(1))
-      {
-        for (MLP_layer in c(2))
-        {
-          tryCatch({
-            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_Individual"))
-            
             #findlag
             lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMAX-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
             result<-MLPX_Individual(preprocessing = preprocessing,
                                     location=location,
                                     denomination=denomination,
-                                    MLP_layer=MLP_layer,flow=flow,
+                                    MLP_layer=MLP_layer,
+                                    flow=flow,
                                     lag=lagrow)
             compiled_result<-rbind(compiled_result,result$modelResult)
             nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
           },error=function(e){
             print(e)
           })
-          
-        }
-      }
-    }
-  }
-}
 
-for (flow in c("Outflow"))
-{
-  for (location in c("Semarang"))
-  {
-    for (denomination in c("K100000"))
-    {
-      for (preprocessing in c(1))
-      {
-        for (MLP_layer in c(1))
-        {
           tryCatch({
-            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_Individual"))
-            
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"ARIMA_MLP_Parallel"))
+
             #findlag
             lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
             result<-ARIMA_MLP_Parallel(preprocessing = preprocessing,
-                                    location=location,
-                                    denomination=denomination,
-                                    MLP_layer=MLP_layer,flow=flow,
-                                    lag=lagrow)
-            compiled_result<-rbind(compiled_result,result$modelResult)
-            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
-          },error=function(e){
-            print(e)
-          })
-          
-        }
-      }
-    }
-  }
-}
-
-for (flow in c("Outflow"))
-{
-  for (location in c("Semarang"))
-  {
-    for (denomination in c("K100000"))
-    {
-      for (preprocessing in c(1))
-      {
-        for (MLP_layer in c(1))
-        {
-          tryCatch({
-            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_Individual"))
-            
-            #findlag
-            lag<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
-            result<-ARIMAX_MLPX_Parallel(preprocessing = preprocessing,
                                        location=location,
                                        denomination=denomination,
-                                       MLP_layer=MLP_layer,flow=flow,
-                                       lag=lag)
+                                       MLP_layer=MLP_layer,
+                                       flow=flow,
+                                       lag=lagrow)
+            compiled_result<-rbind(compiled_result,result$modelResult)
+            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
+          },error=function(e){
+            print(e)
+          })
+
+          tryCatch({
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"ARIMAX_MLPX_Parallel"))
+
+            #findlag
+            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMAX-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
+            result<-ARIMAX_MLPX_Parallel(preprocessing = preprocessing,
+                                         location=location,
+                                         denomination=denomination,
+                                         MLP_layer=MLP_layer,
+                                         flow=flow,
+                                         lag=lagrow)
             compiled_result<-rbind(compiled_result,result$modelResult)
             nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
           },error=function(e){
             print(e)
           })
           
-        }
-      }
-    }
-  }
-}
-
-for (flow in c("Outflow"))
-{
-  for (location in c("Tasikmalaya"))
-  {
-    for (denomination in c("K50000"))
-    {
-      for (preprocessing in c(1))
-      {
-        for (MLP_layer in c(1))
-        {
           tryCatch({
-            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_Individual"))
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"ARIMA_MLP_Seri"))
             
             #findlag
-            lag<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
-            result<-MLP_ARIMA_Series(preprocessing = preprocessing,
-                                         location=location,
-                                         denomination=denomination,
-                                         MLP_layer=MLP_layer,flow=flow,
-                                         lag=lag)
+            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
+            result<-ARIMA_MLP_Series(preprocessing = preprocessing,
+                                     location=location,
+                                     denomination=denomination,
+                                     MLP_layer=MLP_layer,
+                                     flow=flow,
+                                     lag=lagrow)
             compiled_result<-rbind(compiled_result,result$modelResult)
             nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
           },error=function(e){
             print(e)
           })
+          
+          tryCatch({
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"ARIMAX_MLPX_Series"))
+            
+            #findlag
+            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMAX-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
+            result<-ARIMAX_MLPX_Series(preprocessing = preprocessing,
+                              location=location,
+                              denomination=denomination,
+                              MLP_layer=MLP_layer,
+                              flow=flow,
+                              lag=lagrow)
+            compiled_result<-rbind(compiled_result,result$modelResult)
+            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
+          },error=function(e){
+            print(e)
+          })
+          
+          tryCatch({
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLP_ARIMA_Seri"))
+            
+            #findlag
+            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMA-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
+            result<-MLP_ARIMA_Series(preprocessing = preprocessing,
+                                     location=location,
+                                     denomination=denomination,
+                                     MLP_layer=MLP_layer,
+                                     flow=flow,
+                                     lag=lagrow)
+            compiled_result<-rbind(compiled_result,result$modelResult)
+            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
+          },error=function(e){
+            print(e)
+          })
+          
+          tryCatch({
+            print(paste(flow,location,denomination,preprocessing,MLP_layer,Sys.time(),"MLPX_ARIMAX_Series"))
+            
+            #findlag
+            lagrow<-(lag_info %>% filter(Flow==flow,Location==location,Denomination==denomination,Model=="ARIMAX-Individual") %>% select("Lags") %>% slice(1))$Lags[[1]]%>% as.numeric()
+            result<-MLPX_ARIMAX_Series(preprocessing = preprocessing,
+                                       location=location,
+                                       denomination=denomination,
+                                       MLP_layer=MLP_layer,
+                                       flow=flow,
+                                       lag=lagrow)
+            compiled_result<-rbind(compiled_result,result$modelResult)
+            nn_gridsearch_result<-rbind(nn_gridsearch_result,result$gridsearchNN)
+          },error=function(e){
+            print(e)
+          })
+          
           
         }
       }
